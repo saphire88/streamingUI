59da4fed448d5299bd928dab181d2f6ee572a303
saphire88 <cashfire88@gmail.com>
2020-09-08 22:40:43 +0200

    dispatch response from ue4

--- scripts/communication.js
+++ scripts/communication.js
@@ -3,6 +3,14 @@ console.log("TEST")
 let eventReceiver = function (data) {
     console.log("Response received!!!!!!!!!!!!!!!");
     console.log(data)
+    switch(data.object){
+        case 'table':
+            console.log("You entered Table")
+            break;
+        case 'whiteboard':
+            console.log("You entered Whiteboard")
+            break;
+    }
 }
 
 let sendToUE4 = function (data) {
