3fa8e1c069ad33524cae09bfce699e4b0755f32c
dEMillerb <etienne@thegreenbranch.de>
2020-09-10 21:26:35 +0200

    Ui is now working

--- scripts/communication.js
+++ scripts/communication.js
@@ -3,11 +3,14 @@ console.log("TEST")
 let eventReceiver = function (data) {
     console.log("Response received!!!!!!!!!!!!!!!");
     console.log(data)
-    switch(data.object){
+    const jsonData = JSON.parse(data)
+    switch(jsonData.object){
         case 'table':
             console.log("You entered Table")
             break;
         case 'whiteboard':
+            colorPicker()
+            inputWhiteboard()
             console.log("You entered Whiteboard")
             break;
     }
@@ -18,3 +21,26 @@ let sendToUE4 = function (data) {
     emitUIInteraction("From Web");
 }
 
+const colorPicker = function(){
+    let container = document.getElementById('sector321');
+    let Picker =    '<div class="colorpicker-container">'+
+                        '<div class="colorpicker">'+
+                            '<input type="color" id="head" name="head" value="#ffffff">'+
+                            '<label for="head">Change Color</label>'+
+                        '</div>'+
+                    '</div>';
+    container.innerHTML = Picker;
+}
+ const inputWhiteboard = function(){
+    registerKeyboardEvents = function() {}; 
+    let container = document.getElementById('sector231');
+    let Input =     '<div class="whiteboard">'+
+                        '<form action="">'+
+                            '<textarea id="whiteboard" name="whiteboard" rows="4" cols="50">'+
+                                'Enter something to be displayed on the whiteboard'+
+                            '</textarea>'+
+                            '<input type="submit" value="Transfer to Whiteboard">'+
+                        '</form>'+
+                    '</div>';
+    container.innerHTML = Input;
+}
